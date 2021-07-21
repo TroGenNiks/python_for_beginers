c73557b90b6f33af0fcbcd98d9e616e0a6f01fd0
Nisarg <86683043+TroGenNiks@users.noreply.github.com>
2021-07-21 16:43:52 +0530

    initial commit

---
+++ comments.py
@@ -0,0 +1,13 @@
+
+# Name :- Nisdarg keluskar
+"""
+this is a
+multiline comment
+"""
+print("May name is :- Nsarg Anant Keluskar ")
+print("I am in TY")
+
+print("May name is :- Nsarg Anant Keluskar", end=". ")
+print("I am in TY.")
+
+print("escape sequence  \n new line  \t tab ") # comment after statement
