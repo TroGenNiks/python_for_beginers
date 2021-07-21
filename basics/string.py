e99a06cc33457567658210b5f737e1c5d640a5a2
Nisarg <86683043+TroGenNiks@users.noreply.github.com>
2021-07-21 20:35:39 +0530

    operations on string

---
+++ basics/string.py
@@ -0,0 +1,14 @@
+str = "Welcome in my world ."
+print(str)
+print(str[:5]) # print upto 5
+print(str[2:]) # print from 2
+print(str[0::2]) # print by skipping 1 letter
+print(str[::-1])  # reverse the string
+
+# functions of string
+
+print(str.isalnum()) # checking string for alphanumeric or not
+print(str.lower()) # converting into lower
+print(str.upper()) # converting into upper
+print(str.find("in")) # finding inn string
+print(str.replace("in","nisarg in"))  # replacing one letter in string.
