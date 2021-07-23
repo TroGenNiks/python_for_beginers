9d987f68cf86b817ff378148898968fd40093b08
Nisarg <86683043+TroGenNiks@users.noreply.github.com>
2021-07-23 19:26:24 +0530

    if elif and else and in keyword

---
+++ basics/conditionindandlooping.py
@@ -0,0 +1,43 @@
+# if .... else ...... elif
+
+# taking input and checking for greater , smaller , equal
+
+# var = 10
+# print("enter the second number")
+# var1 = int(input()) # taking input in string and typecast in int
+#
+# if var1 > var:
+#     print("var1 is greater")
+# elif var1 < var:
+#     print("var1 is smaller")
+# elif var == var1:
+#     print("both are equale")
+
+
+# in keyword
+# it's working is traversing but returning boolean value
+list = [1,2,3,4,5,6,7,8,9]
+
+# print( 2 in list)
+# if 5 in list:
+#     print("5 is present in list")
+
+# taking user input for age and check eligibility for driving licence
+
+print("enter your age :")
+a = int(input())
+if a < 18:
+    print("you are not eligible for driving")
+elif a == 18:
+    print("you are eligible for learning licence")
+elif a>18:
+    print("u can drive")
+
+
+
+
+
+
+
+
+
